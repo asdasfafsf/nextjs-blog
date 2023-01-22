90f4c5f11e6c4835ad825b2242019e0c24c45b0d
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2023-01-22 12:01:10 +0900

    dynamic route

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,11 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
-// import { getSortedPostsData } from '../lib/posts';
-import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import Date from '../components/date';
 
 
-// export async function getStaticProps() {
-//   const allPostsData = getSortedPostsData();
 
-//   console.log(allPostsData)
-
-//   return {
-//     props: {
-//       allPostsData
-//     }
-//   }
-// }
 
 export async function getStaticProps() {
   const response = await fetch('http://localhost:3000/api/posts')
@@ -55,11 +45,11 @@ export default function Home({allPostsData}) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
