0375d32b6bfd380afcc1d2a0c245df9f642e6332
asdasfafsf <asdasfafsf@naver.com>
2023-01-27 19:38:38 +0900

    errorBoundary

---
+++ components/ErrorBoundary.js
@@ -0,0 +1,47 @@
+class ErrorBoundary extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = { hasError: false, error: null, errorInfo: null };
+    }
+
+
+    static getDerivedStateFromError(error) {
+        console.log(error);
+
+
+        return { hasError: true }
+    }
+
+    componentDidCatch(error, errorInfo) {
+        this.setState({
+            error, errorInfo
+        })
+
+        console.log(`componentDidCatch ${error, errorInfo}`);
+    }
+
+
+    render() {
+       if (this.state.hasError) {
+            if (this.props.fallbackComponent) {
+                return this.props.fallbackComponent;
+            }
+
+            return (
+                <div>
+                    <h2>Error Ocurred</h2>
+                    <details style={{ whiteSpace: 'pre-wrap'}}>
+                        {this.state.error && this.state.error.toString()}
+                        <br />
+                        {this.state.errorInfo.componentStack}                    
+                    </details>
+                </div>
+            )
+       }
+    }
+}
+
+
+export default ErrorBoundary
--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,10 @@ import { useRouter } from 'next/router'
 import { useState } from 'react';
 import { format, formatDistance, formatDistanceToNow } from 'date-fns'
 
+export function reportWebVitals(metric) {
+    console.log(metric);
+}
+
 export default function App({Component, pageProps}) {
     const router = useRouter();
     const [visitedTime] = useState(format(new Date(), 'yyyy-MM-dd HH:mm:ss'));
